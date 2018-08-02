48257e2de4f1f82b8a32ea0ec865bea3bcd4ab43
Slava Kim <slv@mit.edu>
2018-08-01 19:15:12 -0700

    fetch web3 provider from ipc

--- app/client/lib/ethereum/1_web3Init.js
+++ app/client/lib/ethereum/1_web3Init.js
@@ -2,5 +2,13 @@
 if (typeof web3 !== 'undefined') {
   web3 = new Web3(web3.currentProvider);
 } else {
-  web3 = new Web3('ws://localhost:8546');
+  if (window.chrome && window.chrome.ipcRenderer) {
+    web3 = new Web3();
+    window.chrome.ipcRenderer.on('eth-wallet-geth-address', (e, address) => {
+      web3.setProvider(address);
+    });
+    window.chrome.ipcRenderer.send('eth-wallet-get-geth-address');
+  } else {
+    web3 = new Web3('ws://localhost:8546');
+  }
 }
